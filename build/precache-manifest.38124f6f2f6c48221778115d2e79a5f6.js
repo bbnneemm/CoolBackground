65fb299a134a9fcd17b10f835e3dd529c966641c
BNEM <xiaoyu19980217@126.com>
2020-07-10 18:32:53 +0800

    2020-7-10

---
+++ build/precache-manifest.38124f6f2f6c48221778115d2e79a5f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e122ad01e93dad3123b8f54edcaa365a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31aed5ad67d2b304e0ed",
+    "url": "/static/css/main.6ae3ef94.chunk.css"
+  },
+  {
+    "revision": "22773d3a9cb1928b6d14",
+    "url": "/static/js/2.84fa9f31.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.84fa9f31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31aed5ad67d2b304e0ed",
+    "url": "/static/js/main.0f0773ca.chunk.js"
+  },
+  {
+    "revision": "be0681d4fdb5fbb976cf",
+    "url": "/static/js/runtime-main.32381dc2.js"
+  }
+]);
